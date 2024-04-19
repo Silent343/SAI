beb93b29c4778ec69b5d1bb699fda0acc0883341
Silent343 <116988979+Silent343@users.noreply.github.com>
2024-04-18 19:24:44 -0500

    Add files via upload

--- contact_Page/main.js
+++ contact_Page/main.js
@@ -3,3 +3,11 @@ window.addEventListener("scroll", function(){
     header.classList.toggle("sticky", window.scrollY > 0)
 })
 
+const navMenu = document.querySelector(".first-list")
+const backNavBar = document.querySelector(".nav-container")
+const navToggle = document.querySelector(".menuToggle")
+
+navToggle.addEventListener("click", () => {
+    navMenu.classList.toggle("blockft")
+    backNavBar.classList.toggle("bckScroll")
+})
